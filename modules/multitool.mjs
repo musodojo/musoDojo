faa13325a1e9deae63d4c0e1f2382424536a8423
Muso Dojo <musodojomail@gmail.com>
2021-11-14 11:46:28 +0000

    added fade in on addTool

--- modules/multitool.mjs
+++ modules/multitool.mjs
@@ -15,7 +15,6 @@ class Multitool extends Container {
     this.container.addEventListener("removetool", (event) => {
       const TOOL = event.target;
       // fade out
-      TOOL.style.transition = "opacity 0.2s linear";
       TOOL.style.opacity = "0.25";
       // 200ms is same as css transition time in spinningMinusIcon
       setTimeout(() => {
@@ -56,7 +55,15 @@ class Multitool extends Container {
         tool = new FretboardMultitool(PROPS);
     }
     tool.container.style.margin = "1em";
+    // 200ms is same as css transition time in spinningMinusIcon
+    tool.container.style.opacity = "0.25";
+    tool.container.style.transition = "opacity 0.2s linear";
     this.render(tool.container);
+    // needs a small delay before resetting css opacity
+    // otherwise it doesn't transition
+    setTimeout(() => {
+      tool.container.style.opacity = "1";
+    }, 20);
   }
 }
 
