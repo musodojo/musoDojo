5a3c1aebb5f08b5ea783321657e57911b9027616
Muso Dojo <musodojomail@gmail.com>
2022-01-02 19:00:51 +0000

    merge tuning values from fretboardTunings.mjs here

--- modules/data/fretboardInstrumentsProps.mjs
+++ modules/data/fretboardInstrumentsProps.mjs
@@ -1,4 +1,3 @@
-import { FRETBOARD_TUNINGS } from "./fretboardTunings.mjs";
 import { NOTE_SEQUENCES } from "./noteSequences.mjs";
 import { NOTE_COLORS } from "./noteColors.mjs";
 import { NOTE_LABELS } from "./noteLabels.mjs";
@@ -6,17 +5,18 @@ import { COLOR_THEMES } from "./colorThemes.mjs";
 
 const FRET_MARKERS = {
   "Guitar Style": [3, 5, 7, 9, 12, 15, 17, 19, 21, 24],
-  "Ukulele Style": [3, 5, 7, 10, 12, 15],
+  "Mandolin Style": [3, 5, 7, 10, 12, 15, 17, 19, 22, 24],
 };
 
+// see Fretboard class's contstructor for porperty explanations
 const FRETBOARD_INSTRUMENTS_PROPS = {
   defaults: {
     instrument: "Guitar",
-    tuning: FRETBOARD_TUNINGS["Guitar"],
+    tuning: [[40], [45], [50], [55], [59], [64]], // EADGBE
     rootNote: 0,
     sequence: NOTE_SEQUENCES["Mode"]["Ionian / Major"].sequence,
     fromFret: 0,
-    toFret: 21,
+    toFret: 24,
     // mode = "Play" || "Edit One" || "Edit All"
     mode: "Play",
     hand: "Right",
@@ -38,9 +38,9 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
   instruments: {
     Guitar: {
       instrument: "Guitar",
-      tuning: FRETBOARD_TUNINGS["Guitar"],
+      tuning: [[40], [45], [50], [55], [59], [64]], // EADGBE
       fromFret: 0,
-      toFret: 21,
+      toFret: 24,
       showStrings: true,
       showFrets: true,
       fretMarkers: FRET_MARKERS["Guitar Style"],
@@ -49,9 +49,9 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
     },
     "Guitar DADGAD": {
       instrument: "Guitar",
-      tuning: FRETBOARD_TUNINGS["Guitar DADGAD"],
+      tuning: [[38], [45], [50], [55], [57], [62]], // DADGAD
       fromFret: 0,
-      toFret: 21,
+      toFret: 24,
       showStrings: true,
       showFrets: true,
       fretMarkers: FRET_MARKERS["Guitar Style"],
@@ -60,9 +60,9 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
     },
     "Guitar DADGBE": {
       instrument: "Guitar",
-      tuning: FRETBOARD_TUNINGS["Guitar DADGBE"],
+      tuning: [[38], [45], [50], [55], [59], [64]], // Drop D
       fromFret: 0,
-      toFret: 21,
+      toFret: 24,
       showStrings: true,
       showFrets: true,
       fretMarkers: FRET_MARKERS["Guitar Style"],
@@ -71,9 +71,20 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
     },
     Bass: {
       instrument: "Bass",
-      tuning: FRETBOARD_TUNINGS["Bass"],
+      tuning: [[28], [33], [38], [43]], // EADG
       fromFret: 0,
-      toFret: 21,
+      toFret: 24,
+      showStrings: true,
+      showFrets: true,
+      fretMarkers: FRET_MARKERS["Guitar Style"],
+      showFretLabels: true,
+      showFretboardMarkers: true,
+    },
+    "Bass 5-String": {
+      instrument: "Bass",
+      tuning: [[23], [28], [33], [38], [43]], // BEADG
+      fromFret: 0,
+      toFret: 24,
       showStrings: true,
       showFrets: true,
       fretMarkers: FRET_MARKERS["Guitar Style"],
@@ -82,7 +93,7 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
     },
     Violin: {
       instrument: "Violin",
-      tuning: FRETBOARD_TUNINGS["Violin"],
+      tuning: [[55], [62], [69], [76]], // GDAE
       fromFret: 0,
       toFret: 17,
       showStrings: true,
@@ -91,29 +102,71 @@ const FRETBOARD_INSTRUMENTS_PROPS = {
       showFretLabels: true,
       showFretboardMarkers: false,
     },
-    Ukulele: {
-      instrument: "Guitar",
-      tuning: FRETBOARD_TUNINGS["Ukulele"],
-      fromFret: 0,
-      toFret: 17,
-      showStrings: true,
-      showFrets: true,
-      fretMarkers: FRET_MARKERS["Ukulele Style"],
-      showFretLabels: true,
-      showFretboardMarkers: true,
-    },
     Mandolin: {
       instrument: "Guitar",
-      tuning: FRETBOARD_TUNINGS["Mandolin"],
+      tuning: [
+        [55, 55],
+        [62, 62],
+        [69, 69],
+        [76, 76],
+      ], // GDAE
       fromFret: 0,
       toFret: 20,
       showStrings: true,
       showFrets: true,
-      fretMarkers: FRET_MARKERS["Ukulele Style"],
+      fretMarkers: FRET_MARKERS["Mandolin Style"],
       showFretLabels: true,
       showFretboardMarkers: true,
     },
+    Ukulele: {
+      instrument: "Guitar",
+      tuning: [[55], [60], [64], [69]], // GCEA
+      fromFret: 0,
+      toFret: 17,
+      showStrings: true,
+      showFrets: true,
+      fretMarkers: FRET_MARKERS["Mandolin Style"],
+      showFretLabels: true,
+      showFretboardMarkers: true,
+    },
+    //DADGBD: [38, 45, 50, 55, 59, 62], // Double Drop D
+    //EbAbDbGbBbEb: [39, 44, 49, 54, 58, 63], // Half Step Down
+    //DGCFAD: [38, 43, 48, 53, 57, 62], // Whole Step Down
+    //DGDGBD: [38, 43, 50, 55, 59, 62], // Open G
+    //DGDGBbD: [38, 43, 50, 55, 58, 62], // Open G minor
+    //"DADF#AD": [38, 45, 50, 54, 57, 62], // Open D
+    //DADFAD: [38, 45, 50, 53, 57, 62], // Open D minor
   },
 };
 
-export { FRETBOARD_INSTRUMENTS_PROPS };
+function getFretboardInstrumentNameFromTuning(tuningArray) {
+  const VALUE_STRING = JSON.stringify(tuningArray);
+  const TUNINGS = Object.entries(FRETBOARD_INSTRUMENTS_PROPS.instruments);
+  for (const [INSTRUMENT_NAME, INSTRUMENT_PROPS] of TUNINGS) {
+    if (JSON.stringify(INSTRUMENT_PROPS.tuning) === VALUE_STRING) {
+      return INSTRUMENT_NAME;
+    }
+  }
+}
+
+function getFretboardInstrumentSelect(selected = "Guitar") {
+  const SELECT = document.createElement("select");
+  const LABEL = document.createElement("option");
+  LABEL.text = "Instrument";
+  LABEL.disabled = "disabled";
+  SELECT.add(LABEL);
+  Object.keys(FRETBOARD_INSTRUMENTS_PROPS.instruments).forEach((instrument) => {
+    const OPTION = document.createElement("option");
+    OPTION.text = instrument;
+    SELECT.add(OPTION);
+    if (instrument === selected) {
+      OPTION.selected = true;
+    }
+  });
+  return SELECT;
+}
+export {
+  FRETBOARD_INSTRUMENTS_PROPS,
+  getFretboardInstrumentNameFromTuning,
+  getFretboardInstrumentSelect,
+};
